ef4f0cef30d7f9ad193691e8c0b0f75fb10bc43c
planepaper <eddysonkr@gmail.com>
2022-04-01 23:35:40 +0900

    REFACTOR: Move Paddle Variable To Parent Class
    - moveSpeed property should be belong to Paddle Class.

--- Assets/Scripts/ComputerPaddle.cs
+++ Assets/Scripts/ComputerPaddle.cs
@@ -2,4 +2,16 @@ using UnityEngine;
 
 public class ComputerPaddle : Paddle
 {
+    public Rigidbody2D ball;
+
+    private void FixedUpdate()
+    {
+        if (ball.velocity.x > 0f)
+        {
+            if (ball.position.y > transform.position.y)
+            {
+
+            }
+        }
+    }
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     protected Rigidbody2D _rigidbody2D;
+    protected float _moveSpeed = 10f;
 
     private void Awake()
     {
